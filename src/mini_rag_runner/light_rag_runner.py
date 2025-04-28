692badc5c85e2cc7589e9ba1412a6a3f4d63734d
Gordon Watts <gwatts@uw.edu>
2025-04-27 21:17:35 -0700

    Pay attention to the `top_k` parameter (#11)
    * Propagate top_k
    
    * Fix up the response data

--- src/mini_rag_runner/light_rag_runner.py
+++ src/mini_rag_runner/light_rag_runner.py
@@ -72,13 +72,13 @@ def create_app(working_dir: Path, servers: Optional[List[Dict[str, str]]] = None
         r_context = cast(rag_context, fastapi_request.app.state.context)
         if r_context is None:
             raise RuntimeError("The RAG Context is `None` - should never happen.")
-        mode = "mix"
+        mode = "hybrid"
         logging.debug(f"Querying for the question: '{question}'.")
-        q_params = QueryParam(mode=mode, only_need_context=True)
+        q_params = QueryParam(mode=mode, only_need_context=True, top_k=top_k)
         result = r_context.rag.query(question, param=q_params)
         logging.debug(f"  Result: '{result}'.")
         return [
-            RagResponse(chunk=f"Example chunk 1 {result}", document_reference="doc1.pdf"),
+            RagResponse(chunk=f"{result}", document_reference="??"),
         ]
 
     return app
